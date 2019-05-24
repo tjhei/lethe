4ee9a43d48be6e434bb2e666e5e0f4d7c8e43a0e
Bruno Blais <blais.bruno@gmail.com>
2019-05-24 08:23:29 -0400

    - Added a prototype test for the velocity - Remains to be added a prototype test for all the data in the same vector Then finally the composer

--- tests/ib/ib_01.cc
+++ tests/ib/ib_01.cc
@@ -29,82 +29,175 @@
 #include "../tests.h"
 
 
+
 using namespace dealii;
+
+void test1_distance_verification()
+{
+  MPI_Comm                         mpi_communicator(MPI_COMM_WORLD);
+  unsigned int n_mpi_processes (Utilities::MPI::n_mpi_processes(mpi_communicator));
+  unsigned int this_mpi_process (Utilities::MPI::this_mpi_process(mpi_communicator));
+
+  parallel::distributed::Triangulation<2> triangulation (mpi_communicator, typename Triangulation<2>::MeshSmoothing
+                                                         (Triangulation<2>::smoothing_on_refinement | Triangulation<2>::smoothing_on_coarsening));
+  GridGenerator::hyper_cube (triangulation,
+                             -1,1);
+  triangulation.refine_global(2);
+  DoFHandler<2>                  dof_handler(triangulation);
+  FESystem<2>                    fe(FE_Q<2>(2), 1);
+
+  dof_handler.distribute_dofs(fe);
+  DoFRenumbering::Cuthill_McKee(dof_handler);
+
+  IndexSet                         locally_owned_dofs;
+  IndexSet                         locally_relevant_dofs;
+  locally_owned_dofs = dof_handler.locally_owned_dofs ();
+  DoFTools::extract_locally_relevant_dofs (dof_handler,
+                                           locally_relevant_dofs);
+  TrilinosWrappers::MPI::Vector    levelSet_distance;
+  levelSet_distance.reinit (locally_owned_dofs, mpi_communicator);
+  TrilinosWrappers::MPI::Vector    levelSet_velocity;
+  levelSet_velocity.reinit (locally_owned_dofs, mpi_communicator);
+
+  const MappingQ<2>      mapping (1);
+  std::map< types::global_dof_index, Point< 2 > > support_points;
+  //support_points.(locally_owned_dofs.size());
+  DoFTools::map_dofs_to_support_points
+      ( mapping, dof_handler,support_points );
+
+  Point<2> center(0,0);
+  Tensor<1,2> velocity;
+  velocity[0]=1.;
+  velocity[1]=0.;
+  Tensor<1,3> angular;
+  angular[0]=0;
+  angular[1]=0;
+  angular[2]=1;
+  double radius =0.3;
+
+  IBLevelSetCircle<2> circle(center,velocity,angular,radius);
+  auto d = locally_owned_dofs.begin(), enddof=locally_owned_dofs.end();
+  for (; d!=enddof;++d)
+  {
+    levelSet_distance(*d)=circle.distance(support_points[*d]);
+    //levelSet_velocity(*d)=circle.velocity(support_points[*d]);
+  }
+
+  TrilinosWrappers::MPI::Vector    levelSet_distance_global;
+  levelSet_distance_global.reinit(locally_relevant_dofs);
+  levelSet_distance_global=levelSet_distance;
+  d = locally_relevant_dofs.begin();
+  enddof=locally_relevant_dofs.end();
+  for (; d!=enddof;++d)
+  {
+    double x = support_points[*d][0];
+    double y = support_points[*d][1];
+    double analytical_distance = std::sqrt(x*x+y*y)-radius;
+    double imposed_distance = levelSet_distance_global[*d];
+    if (!approximatelyEqual(analytical_distance,imposed_distance,1.e-10))
+    {
+      throw std::runtime_error("Test 1 - Level Set distance is not imposed correctly");
+    }
+  }
+
+  write_ib_scalar_data<2>(triangulation,dof_handler,mpi_communicator,levelSet_distance_global,"ls_distance");
+}
+
+void test2_velocity_verification()
+{
+  MPI_Comm                         mpi_communicator(MPI_COMM_WORLD);
+  unsigned int n_mpi_processes (Utilities::MPI::n_mpi_processes(mpi_communicator));
+  unsigned int this_mpi_process (Utilities::MPI::this_mpi_process(mpi_communicator));
+
+  parallel::distributed::Triangulation<2> triangulation (mpi_communicator, typename Triangulation<2>::MeshSmoothing
+                                                         (Triangulation<2>::smoothing_on_refinement | Triangulation<2>::smoothing_on_coarsening));
+  GridGenerator::hyper_cube (triangulation,
+                             -2,2);
+  triangulation.refine_global(2);
+  DoFHandler<2>                  dof_handler(triangulation);
+  FESystem<2>                    fe(FE_Q<2>(2), 2);
+
+  dof_handler.distribute_dofs(fe);
+  DoFRenumbering::Cuthill_McKee(dof_handler);
+
+  IndexSet                         locally_owned_dofs;
+  IndexSet                         locally_relevant_dofs;
+  locally_owned_dofs = dof_handler.locally_owned_dofs ();
+  DoFTools::extract_locally_relevant_dofs (dof_handler,
+                                           locally_relevant_dofs);
+  TrilinosWrappers::MPI::Vector    levelSet_velocity;
+  levelSet_velocity.reinit (locally_owned_dofs, mpi_communicator);
+
+  const MappingQ<2>      mapping (1);
+  std::map< types::global_dof_index, Point< 2 > > support_points;
+  //support_points.(locally_owned_dofs.size());
+  DoFTools::map_dofs_to_support_points
+      ( mapping, dof_handler,support_points );
+
+  Point<2> center(0,0);
+  Tensor<1,2> velocity;
+  velocity[0]=1.;
+  velocity[1]=0.;
+  Tensor<1,3> angular;
+  angular[0]=0;
+  angular[1]=0;
+  angular[2]=1;
+  double radius =1.;
+
+  IBLevelSetCircle<2> circle(center,velocity,angular,radius);
+  Vector<double> velocity_values(2);
+  //std::cout << "number of dofs " << dof_handler.n_dofs() << std::endl;
+  auto d = locally_owned_dofs.begin(), enddof=locally_owned_dofs.end();
+  for (; d!=enddof;++d)
+  {
+    circle.velocity(support_points[*d],velocity_values);
+    //std::cout << "Pt : " << *d << " - " << support_points[*d] << std::endl;
+    levelSet_velocity[*d]=velocity_values[0];
+    //std::cout << "Velocity value : " << levelSet_velocity[*d] << std::endl;
+    ++d;
+    //std::cout << "Pt : " << *d << " - " << support_points[*d] << std::endl;
+    levelSet_velocity[*d]=velocity_values[1];
+    //std::cout << "Velocity value : " << levelSet_velocity[*d] << std::endl;
+  }
+
+  TrilinosWrappers::MPI::Vector    levelSet_velocity_global;
+  levelSet_velocity_global.reinit(locally_relevant_dofs);
+  levelSet_velocity_global=levelSet_velocity;
+  d = locally_relevant_dofs.begin();
+  enddof=locally_relevant_dofs.end();
+  for (; d!=enddof;++d)
+  {
+    double x = support_points[*d][0];
+    double y = support_points[*d][1];
+    double analytical_velocity_x = -angular[2] * y + velocity[0];
+    double analytical_velocity_y = angular[2] * x + velocity[1];
+
+    double imposed_velocity = levelSet_velocity_global[*d];
+    if (!approximatelyEqual(analytical_velocity_x,imposed_velocity,1.e-10))
+    {
+      throw std::runtime_error("Test 2 - Level Set X velocity is not imposed correctly");
+    }
+    ++d;
+    imposed_velocity = levelSet_velocity_global[*d];
+    if (!approximatelyEqual(analytical_velocity_y,imposed_velocity,1.e-10))
+    {
+      throw std::runtime_error("Test 2 - Level Set Y velocity is not imposed correctly");
+    }
+  }
+
+  write_ib_vector_data<2>(triangulation,dof_handler,mpi_communicator,levelSet_velocity_global,"ls_velocity");
+}
+
 
 int
 main(int argc, char* argv[])
 {
   try
   {
-    initlog();
     Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, numbers::invalid_unsigned_int);
-    MPI_Comm                         mpi_communicator(MPI_COMM_WORLD);
-    unsigned int n_mpi_processes (Utilities::MPI::n_mpi_processes(mpi_communicator));
-    unsigned int this_mpi_process (Utilities::MPI::this_mpi_process(mpi_communicator));
-
-    parallel::distributed::Triangulation<2> triangulation (mpi_communicator, typename Triangulation<2>::MeshSmoothing
-                                                           (Triangulation<2>::smoothing_on_refinement | Triangulation<2>::smoothing_on_coarsening));
-    GridGenerator::hyper_cube (triangulation,
-                               -1,1);
-    triangulation.refine_global(2);
-    DoFHandler<2>                  dof_handler(triangulation);
-    FESystem<2>                    fe(FE_Q<2>(2), 1);
-
-    dof_handler.distribute_dofs(fe);
-    DoFRenumbering::Cuthill_McKee(dof_handler);
-
-    IndexSet                         locally_owned_dofs;
-    IndexSet                         locally_relevant_dofs;
-    locally_owned_dofs = dof_handler.locally_owned_dofs ();
-    DoFTools::extract_locally_relevant_dofs (dof_handler,
-                                             locally_relevant_dofs);
-    TrilinosWrappers::MPI::Vector    levelSet_distance;
-    levelSet_distance.reinit (locally_owned_dofs, mpi_communicator);
-    TrilinosWrappers::MPI::Vector    levelSet_velocity;
-    levelSet_velocity.reinit (locally_owned_dofs, mpi_communicator);
-
-    const MappingQ<2>      mapping (1);
-    std::map< types::global_dof_index, Point< 2 > > support_points;
-    //support_points.(locally_owned_dofs.size());
-    DoFTools::map_dofs_to_support_points
-        ( mapping, dof_handler,support_points );
-
-    Point<2> center(0,0);
-    Tensor<1,2> velocity;
-    velocity[0]=1.;
-    velocity[1]=0.;
-    Tensor<1,3> angular;
-    angular[0]=0;
-    angular[1]=0;
-    angular[2]=1;
-    double radius =0.3;
-
-    IBLevelSetCircle<2> circle(center,velocity,angular,radius);
-    auto d = locally_owned_dofs.begin(), enddof=locally_owned_dofs.end();
-    for (; d!=enddof;++d)
-    {
-      levelSet_distance(*d)=circle.distance(support_points[*d]);
-      //levelSet_velocity(*d)=circle.velocity(support_points[*d]);
-    }
-
-    TrilinosWrappers::MPI::Vector    levelSet_distance_global;
-    levelSet_distance_global.reinit(locally_relevant_dofs);
-    levelSet_distance_global=levelSet_distance;
-    d = locally_relevant_dofs.begin();
-    enddof=locally_relevant_dofs.end();
-    for (; d!=enddof;++d)
-    {
-      double x = support_points[*d][0];
-      double y = support_points[*d][1];
-      double analytical_distance = std::sqrt(x*x+y*y)-radius;
-      double imposed_distance = levelSet_distance_global[*d];
-      if (!approximatelyEqual(analytical_distance,imposed_distance,1.e-10))
-      {
-        throw std::runtime_error("Test 1 - Level Set distance is not imposed correctly");
-      }
-    }
-
-    write_ib_data<2>(triangulation,dof_handler,mpi_communicator,levelSet_distance_global);
+    initlog();
+    test1_distance_verification();
+    test2_velocity_verification();
   }
   catch (std::exception &exc)
   {
--- tests/ib/write_data.h
+++ tests/ib/write_data.h
@@ -26,11 +26,12 @@
 using namespace dealii;
 
 template <int dim>
-void write_ib_data(parallel::distributed::Triangulation<dim> &triangulation,
-               DoFHandler<dim> &dof_handler,
-                MPI_Comm &mpi_communicator,
-               TrilinosWrappers::MPI::Vector &levelSet_vector_global
-               )
+void write_ib_scalar_data(parallel::distributed::Triangulation<dim> &triangulation,
+                   DoFHandler<dim> &dof_handler,
+                   MPI_Comm &mpi_communicator,
+                   TrilinosWrappers::MPI::Vector &levelSet_vector_global,
+                   std::string fname
+                   )
 {
   const MappingQ<dim>      mapping (1);
   DataOut<dim> data_out;
@@ -42,7 +43,109 @@ void write_ib_data(parallel::distributed::Triangulation<dim> &triangulation,
   data_out.add_data_vector (levelSet_vector_global, "Level_Set");
 
   data_out.build_patches (mapping);
-  std::string fname= "level_set";
+  const std::string filename = (fname+
+                                "." +
+                                Utilities::int_to_string
+                                (triangulation.locally_owned_subdomain(), 4));
+  std::ofstream output ((filename + ".vtu").c_str());
+  data_out.write_vtu (output);
+
+  if (Utilities::MPI::this_mpi_process(mpi_communicator) == 0)
+  {
+    std::vector<std::string> filenames;
+    for (unsigned int i=0;
+         i<Utilities::MPI::n_mpi_processes(mpi_communicator);
+         ++i)
+      filenames.push_back (fname+
+                           "." +
+                           Utilities::int_to_string (i, 4) +
+                           ".vtu");
+
+    std::string pvtu_filename = (fname+
+                                 ".pvtu");
+    std::ofstream master_output ((pvtu_filename).c_str());
+    data_out.write_pvtu_record (master_output, filenames);
+
+  }
+}
+
+template <int dim>
+void write_ib_vector_data(parallel::distributed::Triangulation<dim> &triangulation,
+                   DoFHandler<dim> &dof_handler,
+                   MPI_Comm &mpi_communicator,
+                   TrilinosWrappers::MPI::Vector &levelSet_vector_global,
+                   std::string fname
+                   )
+{
+  const MappingQ<dim>      mapping (1);
+  DataOut<dim> data_out;
+  data_out.attach_dof_handler (dof_handler);
+  std::vector<std::string> solution_names (dim, "velocity");
+  std::vector<DataComponentInterpretation::DataComponentInterpretation>
+      data_component_interpretation(dim, DataComponentInterpretation::component_is_part_of_vector);
+  data_out.add_data_vector (levelSet_vector_global, solution_names, DataOut<dim>::type_dof_data, data_component_interpretation);
+
+  Vector<float> subdomain (triangulation.n_active_cells());
+  for (unsigned int i=0; i<subdomain.size(); ++i)
+    subdomain(i) = triangulation.locally_owned_subdomain();
+  data_out.add_data_vector (subdomain, "subdomain");
+
+
+  data_out.build_patches (mapping);
+  const std::string filename = (fname+
+                                "." +
+                                Utilities::int_to_string
+                                (triangulation.locally_owned_subdomain(), 4));
+  std::ofstream output ((filename + ".vtu").c_str());
+  data_out.write_vtu (output);
+
+  if (Utilities::MPI::this_mpi_process(mpi_communicator) == 0)
+  {
+    std::vector<std::string> filenames;
+    for (unsigned int i=0;
+         i<Utilities::MPI::n_mpi_processes(mpi_communicator);
+         ++i)
+      filenames.push_back (fname+
+                           "." +
+                           Utilities::int_to_string (i, 4) +
+                           ".vtu");
+
+    std::string pvtu_filename = (fname+
+                                 ".pvtu");
+    std::ofstream master_output ((pvtu_filename).c_str());
+    data_out.write_pvtu_record (master_output, filenames);
+
+  }
+}
+
+template <int dim>
+void write_ib_data(parallel::distributed::Triangulation<dim> &triangulation,
+                   DoFHandler<dim> &dof_handler,
+                   MPI_Comm &mpi_communicator,
+                   TrilinosWrappers::MPI::Vector &levelSet_vector_global,
+                   std::string fname
+                   )
+{
+  const MappingQ<dim>      mapping (1);
+  DataOut<dim> data_out;
+  data_out.attach_dof_handler (dof_handler);
+  std::vector<std::string> solution_names ("distance");
+  std::vector<DataComponentInterpretation::DataComponentInterpretation>
+          data_component_interpretation(1,DataComponentInterpretation::component_is_scalar);
+  for (int i = 0 ; i <dim ; ++i)
+  {
+    solution_names.push_back("velocity");
+    data_component_interpretation.push_back(DataComponentInterpretation::component_is_part_of_vector);
+  }
+  data_out.add_data_vector (levelSet_vector_global, solution_names, DataOut<dim>::type_dof_data, data_component_interpretation);
+
+  Vector<float> subdomain (triangulation.n_active_cells());
+  for (unsigned int i=0; i<subdomain.size(); ++i)
+    subdomain(i) = triangulation.locally_owned_subdomain();
+  data_out.add_data_vector (subdomain, "subdomain");
+
+
+  data_out.build_patches (mapping);
   const std::string filename = (fname+
                                 "." +
                                 Utilities::int_to_string
