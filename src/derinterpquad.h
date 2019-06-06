9f58db2bc9c8056bf6c009a66bf21e7f010736aa
Antoine Avrit <avrit@ensta.fr>
2019-06-06 13:59:23 -0400

    ajout fichier renvoyant la valeur des gradients de chaque fonction d,interpolation dans un quadrilatère

--- src/derinterpquad.h
+++ src/derinterpquad.h
@@ -1,4 +1,22 @@
-#ifndef DERINTERPQUAD_H
-#define DERINTERPQUAD_H
-
-#endif // DERINTERPQUAD_H
+void derinterpquad(int i, double x, double y, double grad[2])
+{
+    if (i==0)
+    {
+        grad[0]=(1+y)/4;
+        grad[1]=(1+x)/4;
+    }
+    else if (i==1)
+    {
+        grad[0]=-(1+y)/4;
+        grad[1]=(1-x)/4;
+    }
+    else if (i==2)
+    {
+        grad[0]=-(1-y)/4;
+        grad[1]=-(1-x)/4;
+    }
+    else {
+        grad[0]=(1-y)/4;
+        grad[1]=-(1+x)/4;
+    }
+}
