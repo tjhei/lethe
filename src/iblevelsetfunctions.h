4a4ca7b7921765f012e2a99edbf659a3a637f539
Bruno Blais <blais.bruno@gmail.com>
2019-06-18 08:35:16 -0400

    - Added a plane IB function and a unit test for it

--- src/iblevelsetfunctions.h
+++ src/iblevelsetfunctions.h
@@ -107,14 +107,17 @@ public:
 };
 
 
+// Level Set function for a plane
+// center is a point lying on the plane
+// normal is the outward normal of the plane
+
 template <int dim>
 class IBLevelSetPlane: public IBLevelSetFunctions<dim>
-{ // ligne verticale ayant pour abscisse "abscisse"
+{
 private:
   Point<dim>    center;
   Tensor<1,dim> normal;
   Tensor<1,dim> linear_velocity;
-  double norm_normal;
 public:
     IBLevelSetPlane(Point<dim> p_center, Tensor<1,dim> p_normal, Tensor<1,dim> p_linear_velocity, double p_scal):
       IBLevelSetFunctions<dim>(p_scal),
@@ -122,19 +125,18 @@ public:
       normal(p_normal),
       linear_velocity(p_linear_velocity)
     {
-      norm_normal=normal.norm();
+      // Normalise normal vector
+      normal = normal / normal.norm();
     }
 
     // Value of the distance
     virtual double distance(const Point<dim> &p)
     {
-      // Get the vector between p and the center
-      const Point<dim> vec=p-center;
-      return vec * normal/norm_normal;
+      return ((p-center) * normal);
     }
 
     // Value of the velocity
-    virtual void   velocity(const Point<dim> /*&p*/, Vector<double> &values)
+    virtual void velocity(const Point<dim> &p, Vector<double> &values)
     {
         for (int i = 0 ; i<dim ; ++i)
         {
@@ -143,24 +145,4 @@ public:
     }
 };
 
-//template<int dim>
-//void IBLevelSetFunction<dim>::vector_value(const Point<dim> &p,
-//                                           Vector<double> &values) const
-//{
-//    assert(dim==2);
-//    const double a = M_PI;
-//    const double x = p[0];
-//    const double y = p[1];
-//        values(0) = (2*a*a*(-sin(a*x)*sin(a*x) +
-//                            cos(a*x)*(cos(a*x)))*sin(a*y)*cos(a*y)
-//                     - 4*a*a*sin(a*x)*sin(a*x)*sin(a*y)*cos(a*y)
-//                     - 2.0*x)*(-1.)
-//                + a*std::pow(sin(a*x),3.) * std::pow(sin(a*y),2.) * std::cos(a*x);
-//        values(1) = (2*a*a*(sin(a*y)*(sin(a*y)) - cos(a*y)*cos(a*y))
-//                     *sin(a*x)*cos(a*x) + 4*a*a*sin(a*x)*sin(a*y)*sin(a*y)
-//                     *cos(a*x) - 2.0*y)*(-1)
-//                + a*std::pow(sin(a*x),2.) * std::pow(sin(a*y),3.) * std::cos(a*y);
-//
-//}
-//
 #endif
