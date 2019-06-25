244d56ba23524268f0072c16c71ed911155ba02f
Antoine Avrit <avrit@ensta.fr>
2019-06-25 16:39:12 -0400

    all the cases where the absolute value of the distance for a vertex is inferior to 1e-4 are handled by considering the vertex is in the solid, which brings a little error but allows the programms to work no matter what

--- src/nouvtriangles.h
+++ src/nouvtriangles.h
@@ -70,13 +70,12 @@ void nouvtriangles(std::vector<int> &corresp, std::vector<In_fluid_or_in_solid> 
         //std::cout << coor_elem[jj] << std::endl;
     }
 
-    double accuracy = 0.0000000001;
+    double accuracy = 0.0001;
 
     // We will start by finding if there are any changes in the sign of the distance function among the the summits
 
     std::vector<int> a(4);
     int sum_a =0;
-    int in = 0; // it will allow us to spot the hypothetical cases where the boundary is one of the sides of the element considered
     for (int i = 0; i < 4 ; i++)
     {
         a[i]=1;
@@ -85,8 +84,9 @@ void nouvtriangles(std::vector<int> &corresp, std::vector<In_fluid_or_in_solid> 
 
         else if ((val_f[i]<accuracy) && (val_f[i]>-accuracy))
         {
-            a[i]=0;
-            if (((val_f[(i+1) % 4] <accuracy) && (val_f[i]>-accuracy)) || ((val_f[(i+3) % 4]<accuracy)&&(val_f[(i+3) % 4]>-accuracy))) {in = 1;}
+            a[i]=-1;
+            val_f[i] = -accuracy;
+            No_pts_solid[vec_change_coor[i]] = solid;
         }
         sum_a += a[i];
     }
@@ -96,31 +96,12 @@ void nouvtriangles(std::vector<int> &corresp, std::vector<In_fluid_or_in_solid> 
     if (sum_a==0) // there are as many summits in the fluid as in the solid
     {
 
-         // we have to look for the cases where the boundary fluid-solid passes through summits
         for (int i = 0; i < 4 ; i++)
         {
 
             if (a[i]>0)
             {
-                if (a[(i+1) % 4]==0) // we are in the case where the boundary goes through 2 summits
-                {
-                    decomp_elem[0]=coor_elem[i];
-                    decomp_elem[1]=coor_elem[(i+1) % 4];
-                    decomp_elem[2]=coor_elem[(i+3) % 4];
-
-                    corresp = {vec_change_coor[i], vec_change_coor[(i+1)%4], vec_change_coor[(i-1)%4], -1, -1, -1, -1, -1, -1};
-                    num_elem = {coor_elem1[0], coor_elem1[1], coor_elem1[2], coor_elem1[3], coor_elem[(i+1) % 4], coor_elem[(i+3) % 4]}; //// il va falloir traiter ce cas autrement ...
-                     ////////
-                    ///    ///
-                    /// // ///
-                    /// // ///
-                    ///    ///
-                     ////////
-                    *nb_poly = 1;
-                    break;
-                }
-
-                else if ((i==0) && (a[3]>0)) // this case exists just to have always the same order in the summits of the sub-elements
+                if ((i==0) && (a[3]>0)) // this case exists just to have always the same order in the summits of the sub-elements
                 {
                     quadboundary(4, decomp_elem, coor_elem, val_f);
 
@@ -148,8 +129,6 @@ void nouvtriangles(std::vector<int> &corresp, std::vector<In_fluid_or_in_solid> 
 
     else if (sum_a<0 && sum_a>-3) // case where there is one or less summit in the fluid part
     {
-        if (in==0) // if in = 1, it means that the boundary is merging with one side
-        {
             int b=0;
             for (int i = 0; i < 4; ++i)
             {
@@ -159,19 +138,14 @@ void nouvtriangles(std::vector<int> &corresp, std::vector<In_fluid_or_in_solid> 
             std::vector<Point<2> >      boundary_pts(2);
             trgboundary(b, boundary_pts, coor_elem, val_f);
 
-            //std::cout << "Boundary Points (" << boundary_pts[0] << "), (" << boundary_pts[1] << ")" << std::endl;
             decomp_elem[0]=coor_elem[b];
             decomp_elem[1]=boundary_pts[0];
             decomp_elem[2]=boundary_pts[1];
-            //std::cout <<  vec_change_coor[b] << std::endl;
+
             num_elem = {coor_elem1[0], coor_elem1[1], coor_elem1[2], coor_elem1[3], boundary_pts[0], boundary_pts[1]};
             corresp = {vec_change_coor[b], 4, 5, -1,-1,-1,-1,-1};
 
-            *nb_poly = 1;
-        }
-
-        else if (in==1){*nb_poly = 0;}
-
+            *nb_poly = 1;   
     }
 
 
@@ -188,11 +162,9 @@ void nouvtriangles(std::vector<int> &corresp, std::vector<In_fluid_or_in_solid> 
 
         if (sum_a==2)
         {
-            if (in==0)
-            {
+
                 std::vector<Point<2> >      boundary_pts(2);
                 trgboundary(b, boundary_pts, coor_elem, val_f);
-                //std::cout <<"Boundary Points (" << boundary_pts[0] << "), (" << boundary_pts[1] << ")" << std::endl;
                 decomp_elem [0] = coor_elem[(b+1)%4];
                 decomp_elem [1] = coor_elem[(b+2)%4];
                 decomp_elem [2] = boundary_pts[0];
@@ -209,49 +181,7 @@ void nouvtriangles(std::vector<int> &corresp, std::vector<In_fluid_or_in_solid> 
                 num_elem = {coor_elem1[0], coor_elem1[1], coor_elem1[2], coor_elem1[3], boundary_pts[1], boundary_pts[0]}; // boundary points are sorte differently because of the way the function trgboundary works
 
                 *nb_poly = 3;
-            }
-            else if (in==1){*nb_poly = 0;}
-
-        }
-
-        else if (sum_a==1) // corresponds to the case where there is one summit in the solid and one summit crossed by the boundary
-        {
-            std::vector<Point<2> >      boundary_pts(2);
-            trgboundary(b, boundary_pts, coor_elem, val_f);
-
-            if ((val_f[(b+1) % 4]<accuracy) && (val_f[(b+1) % 4]>-accuracy))
-            {
-                decomp_elem[0] = coor_elem[(b+2) % 4];
-                decomp_elem[1] = coor_elem[(b+3) % 4];
-                decomp_elem[2] = boundary_pts[0];
-                decomp_elem[3] = boundary_pts[1];
-
-                corresp = {vec_change_coor[(b+2) % 4], vec_change_coor[(b+3) % 4], vec_change_coor[(b+1) % 4], 4,-1,-1,-1,-1,-1};
-                num_elem = {coor_elem1[0], coor_elem1[1], coor_elem1[2], coor_elem1[3], decomp_elem[3], decomp_elem[2]};
-
-            }
-
-            else if ((val_f[(b+3) % 4]<accuracy) && (val_f[(b+3) % 4]>-accuracy))
-            {
-                decomp_elem[0] = coor_elem[(b+1) % 4];
-                decomp_elem[1] = coor_elem[(b+2) % 4];
-                decomp_elem[2] = boundary_pts[0];
-                decomp_elem[3] = boundary_pts[1];
-
-                corresp = {vec_change_coor[(b+1) % 4], vec_change_coor[(b+2) % 4], 5, vec_change_coor[(b+3) % 4], -1,-1,-1,-1,-1};
-                num_elem = {coor_elem1[0], coor_elem1[1], coor_elem1[2], coor_elem1[3], decomp_elem[3], decomp_elem[2]};
-
-            }
-
-
-
-
-
-            *nb_poly = -1;
         }
     }
-    /*if (*nb_poly > 0) {for (int i = 0; i < *nb_poly; ++i) {
-        std::cout <<"Sub_elem " << i+1 <<" = " << decomp_elem[3*i] << "\n" << decomp_elem[3*i+1] << "\n" << decomp_elem[3*i+2] << "\n" <<std::endl;
-    } } */
 }
 #endif //NOUVTRIANGLES_H
